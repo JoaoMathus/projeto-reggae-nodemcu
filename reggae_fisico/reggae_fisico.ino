bdfc2a7011c346b1c218ea5a74f8c90f2831461e
JoaoMathus <joaomatheusgdo@gmail.com>
2025-05-17 10:18:51 -0300

    Lógica básica de acionamento do temporizador de exposição ao sol implementada.

--- reggae_fisico/reggae_fisico.ino
+++ reggae_fisico/reggae_fisico.ino
@@ -1,3 +1,9 @@
+/*
+ O seguintes dados virão por MQTT:
+  sinal_de_acionamento_do_temporizador; e
+	segundos_ao_sol.
+*/
+
 #include <DHT.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
@@ -15,6 +21,9 @@ DHT dht(PINO_DO_DHT, TIPO_DO_DHT);
 
 Adafruit_SH1106G display(LARGURA_DA_TELA, ALTURA_DA_TELA, &Wire, -1);
 
+bool sinal_de_acionamento_do_temporizador = true; /* valor temporário */
+int segundos_ao_sol = 3; /* valor temporário */
+
 void setup() {
 	Serial.begin(115200);
 
@@ -31,6 +40,10 @@ void setup() {
 	display.setTextSize(1);
 	display.setTextColor(SH110X_WHITE);
 	display.display();
+}
+
+int segundos_para_milissegundos(int segundos) {
+	return segundos * 1000; /* 1s = 1000ms */
 }
 
 void mostrar_temperatura_e_umidade(float temperatura, int umidade) {
@@ -60,6 +73,20 @@ void mostrar_aviso() {
 	display.display();
 }
 
+void mostrar_comeco_do_tempo_ao_sol(int segundos) {
+	for (int i = segundos; i > 0; i--) {
+		display.clearDisplay();
+		display.setCursor(0, 10);
+		display.println("Comeco de exposicao");
+		display.setCursor(0, 20);
+		display.println("por " + String(i));
+		display.setCursor(0, 30);
+		display.println("segundos");
+		display.display();
+		delay(segundos_para_milissegundos(i));
+	}
+}
+
 void mostrar_fim_do_tempo_ao_sol() {
 	display.clearDisplay();
 	display.setCursor(0, 10);
@@ -73,10 +100,14 @@ void loop() {
 	int umidade_do_solo = analogRead(A0);
 	float temperatura = dht.readTemperature();
 
+	if (sinal_de_acionamento_do_temporizador) {
+		mostrar_comeco_do_tempo_ao_sol(segundos_ao_sol);
+		mostrar_fim_do_tempo_ao_sol();
+		delay(2000);
+	}
+
 	mostrar_temperatura_e_umidade(temperatura, umidade_do_solo);
 	delay(2000);
 	mostrar_aviso();
-	delay(2000);
-	mostrar_fim_do_tempo_ao_sol();
 	delay(2000);
 }
